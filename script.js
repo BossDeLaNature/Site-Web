252f82fa8625c6f200f6fa24bbaa35244e5ebbd4
Medjahed Haris <haris.medjahed@inserm.fr>
2024-06-23 10:47:03 +0200

    génie

--- script.js
+++ script.js
@@ -37,8 +37,10 @@ fetch('Excel/Question-Chapitre-II.json')
 
     // Fonction pour vérifier la réponse
     function checkAnswer(selectedAnswer, correctAnswer, justification) {
+      let numeroquestion = num.toString(quizData[currentQuestionIndex]['Numero']);
+      let imagesource = 'media/dias/'.concat(numeroquestion, '.png')
       const imageContainer = document.getElementById('image-container');
-      imageContainer.innerHTML = '<button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal" data-whatever="@mdo"> <img src="media/dias/1.png" class="w-100" alt="Responsive image"/> </button>';
+      imageContainer.innerHTML = '<button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModal" data-whatever="@mdo"> <img src="'.concat(imagesource,''" class="w-100" alt="Responsive image"/> </button>');
         if (selectedAnswer === correctAnswer) {
             alert(`Correct! Justification: ${justification}`);
         } else {
