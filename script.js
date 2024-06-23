0db820aac4448d71ece0c77a24938144ee30e3f7
Medjahed Haris <haris.medjahed@inserm.fr>
2024-06-23 14:54:05 +0200

    chui un psychopath

--- script.js
+++ script.js
@@ -74,27 +74,13 @@ fetch('Excel/Question-Chapitre-II.json')
 
     //Menu déroulant
     function Menuderoulant(){
-      let diviseur = 0;
-      for (diviseur; diviseur < Math.floor((quizData.length-1)/10);diviseur++){
-        const ligne = document.createElement('row');
-        for (let pas = 0; pas < 10; pas++) {
-          const quizContainer = document.getElementById('menu-container');
-          const choixButton = document.createElement('button');
-          choixButton.className = 'btn btn-success'
-          choixButton.textContent = 'Question '.concat(quizData[pas]['Numero'].toString());
-          choixButton.addEventListener('click', () => Ajoutquestion(pas));
-          ligne.appendChild(choixButton);
-        }
-        quizContainer.appendChild(ligne);
-      }
-      for (let pas = 0; pas <quizData.length%10 ; pas++) {
-        const ligne = document.createElement('row');
-        const quizContainer = document.getElementById('menu-container');
+      for (let pas = 0; pas <quizData.length ; pas++) {
+        const menuContainer = document.getElementById('menu-container');
         const choixButton = document.createElement('button');
         choixButton.className = 'btn btn-success'
         choixButton.textContent = 'Question '.concat(quizData[pas]['Numero'].toString());
         choixButton.addEventListener('click', () => Ajoutquestion(pas));
-        ligne.appendChild(choixButton);
+        menuContainer.appendChild(choixButton);
       }
     }
 
