ce6ce506ce9f91094b7ece0b966eff3dc46055b0
Medjahed Haris <haris.medjahed@inserm.fr>
2026-06-05 23:08:27 +0200

    pathoinf

---
+++ Pathologie Infectieuse/data/Transform.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 13:03:55 2026
+
+@author: moito
+"""
+
+import pandas as pd
+from json import loads, dumps
+import json
+# Charger le fichier Excel
+
+import os
+import sys
+
+# --- CONFIGURATION DU DOSSIER ---
+# Force Python à travailler dans le dossier du script
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+Cardio = "cardio_"
+
+
+
+
+for i in range(3):
+    a = str(i+1)
+    df = pd.read_excel("Annale "+a+".xlsx")
+
+    df = df.fillna("")
+
+
+    df["Reponse"] = df["Reponse"]==1.0
+
+    df["image"] = "media/annale " +a+"/"+df["image"].astype(str)+".png"
+
+    # Convertir en JSON
+    json_data = df.to_json(orient="records",indent=4)
+
+    parsed = loads(json_data)
+
+    test = dumps(parsed, indent=4, separators=(',', ': '))
+    test2 = loads(test)
+
+
+
+    # Sauvegarder en fichier JSON
+
+        
+    with open("Annale "+a+".json", "w", encoding="utf-8") as f:
+        json.dump(test2, f, ensure_ascii=False, indent=2)
